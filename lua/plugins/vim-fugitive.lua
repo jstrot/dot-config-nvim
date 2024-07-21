67c6358fc4b6e1c915d2a257912187b052f78d43
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-07-20 21:37:25 -0400

    vim-fugitive: Add Gclogfunc and Gllogfunc commands

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,6 +2,18 @@
 return {
   {
     "tpope/vim-fugitive",
+    config = function()
+      -- require('fugitive')  -- Auto-loading
+
+      -- https://github.com/tpope/vim-fugitive/issues/1663
+      vim.api.nvim_create_user_command('Gclogfunc', function()
+        vim.cmd [[ execute '.Gclog -L :' . expand('<cword>') . ':%' ]]
+      end, { bang = true, bar = true, })
+      vim.api.nvim_create_user_command('Gllogfunc', function()
+        vim.cmd [[ execute '.Gclog -L :' . expand('<cword>') . ':%' ]]
+      end, { bang = true, bar = true, })
+
+    end
   }
 }
 
