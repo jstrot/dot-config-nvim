3280f45ed38bd18436354b640260876d2fb96efa
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-07-30 16:35:32 -0400

    todo-comments: Add standard and custom examples

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -7,7 +7,26 @@ return {
       'nvim-lua/plenary.nvim',
     },
     opts = {
-      signs = false,
+      signs = false,  -- Enable this if you want to see signs in the margin
+      keywords = {
+        -- Standard keywords:
+        -- FIX: Looks like this
+        --  FIXME: is an alternate
+        --  BUG: is an alternate
+        --  FIXIT: is an alternate
+        --  ISSUE: is an alternate
+        -- TODO: Looks like this
+        -- HACK: Looks like this
+        -- WARN: Looks like this
+        -- PERF: Looks like this
+        -- NOTE: Looks like this
+        -- TEST: Looks like this
+
+        -- Add your own:
+        -- XXXJST: Looks like this
+        XXXJST = { icon = '😎', color = 'warning', alt = {} },
+      },
+
     },
   },
 }
