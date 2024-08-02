319105952221683466d2fd35565052f4754f67f6
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-08-02 02:40:25 -0400

    Complete nvim-coverage support

--- lua/plugins/nvim-coverage.lua
+++ lua/plugins/nvim-coverage.lua
@@ -8,22 +8,21 @@ return {
     -- https://github.com/andythigpen/nvim-coverage/pull/44
     'https://github.com/strottie/nvim-coverage', branch = 'strottie-cpp-cobertura',
 
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    rocks = { 'lua-xmlreader' },
-    init = function()
-      require('coverage').setup({
-        lang = {
-          cpp = {
-            coverage_file = "report.info",
-          },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      lang = {
+        cpp = {
+          coverage_file = 'report.info',
         },
-        signs = {
-          covered = { hl = 'CoverageCovered', text = '✔' },
-          uncovered = { hl = 'CoverageUncovered', text = '✖' },
-          partial = { hl = 'CoveragePartial', text = '◔' },
-        },
-      })
-    end,
+      },
+      signs = {
+        covered = { hl = 'CoverageCovered', text = '✔' },
+        uncovered = { hl = 'CoverageUncovered', text = '✖' },
+        partial = { hl = 'CoveragePartial', text = '◔' },
+      },
+    },
   },
 }
 
