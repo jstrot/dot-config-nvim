bf560ad08b56375624c0f38cef0a872073d6d3c2
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-08-12 20:30:49 -0400

    Mason: Enable auto installation using mason-null-ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,5 @@
--- https://github.com/williamboman/mason.nvim
 return {
+  -- https://github.com/williamboman/mason.nvim
   {
     'williamboman/mason.nvim',
     opts = {
@@ -12,12 +12,25 @@ return {
       }
     }
   },
+  -- https://github.com/williamboman/mason-lspconfig.nvim
   {
     'williamboman/mason-lspconfig.nvim',
     dependencies = {
       'williamboman/mason.nvim',
     },
     opts = {
+    }
+  },
+  -- https://github.com/jay-babu/mason-null-ls.nvim
+  {
+    'jay-babu/mason-null-ls.nvim',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    opts = {
+      automatic_installation = true,
     }
   }
 }
