983a8b7b35d7056596b7520be53fad5f29877f18
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-09-02 20:25:38 -0400

    Plugins: Add oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+-- https://github.com/stevearc/oil.nvim
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = {
+      -- 'echasnovski/mini.icons',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+    },
+  },
+}
+
+-- vim: sw=2 et
