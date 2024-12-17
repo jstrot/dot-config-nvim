c44f8803c15334676a7cb4f6461e6e44c65dcf59
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-12-17 17:17:17 -0500

    (Alpha) images: Add support for mermaid/plantuml/d2 diagrams

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -47,6 +47,41 @@ return {
     },
   },
 
+  -- https://github.com/3rd/diagram.nvim
+  {
+    '3rd/diagram.nvim',
+    dependencies = {
+      "3rd/image.nvim",
+    },
+    enabled = false, -- You need `mmdc` (mermaid), `plantuml`, and `d2` (ditaa) installed.
+    lazy = not (_3rd_image_processor and _3rd_backend),
+    config = function()
+      require("diagram").setup({
+        integrations = {
+          require("diagram.integrations.markdown"),
+          require("diagram.integrations.neorg"),
+        },
+        renderer_options = {
+          -- mermaid = {
+          --   background = nil, -- nil | "transparent" | "white" | "#hex"
+          --   theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          --   scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          -- },
+          -- plantuml = {
+          --   charset = nil,
+          -- },
+          -- d2 = {
+          --   theme_id = nil,
+          --   dark_theme_id = nil,
+          --   scale = nil,
+          --   layout = nil,
+          --   sketch = nil,
+          -- },
+        }
+      })
+    end,
+  },
+
 }
 
 --[[
