61c93c981c50f4b890bdc5aa06c7da1d161ec90c
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-12-17 16:53:05 -0500

    CopilotChat: Moving to main branch -- canary is deprecated

--- lua/plugins/CopilotChat.lua
+++ lua/plugins/CopilotChat.lua
@@ -2,7 +2,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     event = 'VeryLazy',
     dependencies = {
       -- { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
