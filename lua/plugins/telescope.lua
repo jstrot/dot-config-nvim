66068f6a9ab232770344433ec71c0f7c85cd9f38
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-12-21 01:26:25 -0500

    Enhance emoji searching to reduce the sources based on file type

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -212,7 +212,23 @@ return {
     },
     config = function()
       pcall(require('telescope').load_extension, 'symbols')
-      vim.keymap.set('n', '<leader>se', '<cmd>Telescope symbols<CR>', { desc = '[S]earch Symbols/[E]mojis' })
+      vim.keymap.set('n', '<leader>se', function ()
+        local sources = {
+            'emoji',
+            'kaomoji',
+            'gitmoji',
+            'math',
+            -- 'latex',
+        }
+        if vim.bo.filetype == 'gitcommit' then
+          sources = { 'gitmoji' }
+        elseif vim.bo.filetype == 'tex' or vim.bo.filetype == 'plaintex' or vim.bo.filetype == 'context' then
+          sources = { 'latex' }
+        end
+        require('telescope.builtin').symbols{
+          sources = sources,
+        }
+      end, { desc = '[S]earch Symbols/[E]mojis' })
     end,
   },
   -- https://github.com/nvim-telescope/telescope-bibtex.nvim
