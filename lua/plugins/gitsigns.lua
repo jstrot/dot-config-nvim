6e197dd78f847bf7fc4c58a309f52aa84c8ef760
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-01-14 22:13:30 -0500

    gitsigns: Expose some of the default configs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,6 +4,16 @@ return {
     "lewis6991/gitsigns.nvim",
     event = 'VeryLazy',
     opts = {
+
+      signs_staged_enable = true,
+      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame` (<leader>tgb)
+
+      attach_to_untracked = false,
+
       on_attach = function(bufnr)
         local gitsigns = require('gitsigns')
 
