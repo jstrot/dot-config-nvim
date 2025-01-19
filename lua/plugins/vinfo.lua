a0bb9b8344ae6e9e8c20893f25d398c278d33ab7
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-01-18 22:51:06 -0500

    Add Vinfo plugin

---
+++ lua/plugins/vinfo.lua
@@ -0,0 +1,15 @@
+-- https://github.com/alx741/vinfo
+return {
+  {
+    "alx741/vinfo",
+    -- event = "VeryLazy",
+    cmd = {
+      "Vinfo",
+      "VinfoC",
+      -- "VinfoN", "VinfoNext",
+      -- "VinfoP", "VinfoPrevious",
+    },
+  },
+}
+
+-- vim: sw=2 et
