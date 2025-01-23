bf17b7b7fc9bbc7442c0f972a647ab1ba62ad60a
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-01-23 18:31:52 -0500

    Keymaps: <leader>fl: [F]ormat using [L]SP

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -57,55 +57,64 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
+
           -- NOTE: Remember that Lua is a real programming language, and as such it is possible
           -- to define small helper and utility functions so you don't have to repeat yourself.
           --
           -- In this case, we create a function that lets us more easily define mappings specific
           -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          local map = function(modes, lhs, rhs, opts)
+            opts = opts or {}
+            setmetatable(opts,{__index={
+              buffer = event.buf,
+            }})
+            vim.keymap.set(modes, lhs, rhs, opts)
           end
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('n', 'gd', require('telescope.builtin').lsp_definitions, {desc='LSP: [G]oto [D]efinition'})
 
           -- Find references for the word under your cursor.
-          map('<leader>slr', require('telescope.builtin').lsp_references, '[S]earch [L]SP [R]eferences')
+          map('n', '<leader>slr', require('telescope.builtin').lsp_references, {desc='LSP: [S]earch [L]SP [R]eferences'})
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('<leader>sli', require('telescope.builtin').lsp_implementations, '[S]earch [L]SP [I]mplementations')
+          map('n', '<leader>sli', require('telescope.builtin').lsp_implementations, {desc='LSP: [S]earch [L]SP [I]mplementations'})
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, {desc='LSP: Type [D]efinition'})
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, {desc='LSP: [D]ocument [S]ymbols'})
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, {desc='LSP: [W]orkspace [S]ymbols'})
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('n', '<leader>rn', vim.lsp.buf.rename, {desc='LSP: [R]e[n]ame'})
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('n', '<leader>ca', vim.lsp.buf.code_action, {desc='LSP: [C]ode [A]ction'})
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('n', 'K', vim.lsp.buf.hover, {desc='LSP: Hover Documentation'})
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('n', 'gD', vim.lsp.buf.declaration, {desc='LSP: [G]oto [D]eclaration'})
+
+          -- Opens a popup that displays documentation about the word under your cursor
+          --  See `:help K` for why this keymap.
+          map({'n', 'v'}, '<leader>fl', vim.lsp.buf.format, {desc='LSP: [F]ormat using [L]SP'})
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -144,9 +153,9 @@ return {
             -- NOTE: The default mapping from kickstart.nvim is `<leader>th`
             -- but here's I'm using `<leader>tlh` to match the mappings from
             -- toggle-lsp-diagnostics.nvim.
-            map('<leader>tlh', function()
+            map('n', '<leader>tlh', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-            end, '[T]oggle [L]SP diagnostics inlay [H]ints')
+            end, {desc='LSP: [T]oggle [L]SP diagnostics inlay [H]ints'})
           end
 
           local name = client and client.name
@@ -189,10 +198,10 @@ return {
         -- tsserver = {},
         --
 
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/pyright.lua
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/pyright.lua
         pyright = {},
 
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/clangd.lua
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/clangd.lua
         clangd = {
           filetypes = { 'c', 'cpp', 'cc', },
           offset_encoding = 'utf-16',
@@ -216,7 +225,7 @@ return {
           end,
         },
 
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/lua_ls.lua
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -261,7 +270,7 @@ return {
           },
         },
 
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/bashls.lua
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/bashls.lua
         bashls = {
         }
 
@@ -270,7 +279,7 @@ return {
         -- TODO: XXXJST This is broken when using mason-tool-installer
 
         ccls = {
-          -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/ccls.lua
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/ccls.lua
           lsp = {
             lspcopnfig = {
               filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'opencl' },
