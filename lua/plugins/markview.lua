caee61bf985ae2fa6eaf356c82b3e5e8da46bedd
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-01-25 18:30:40 -0500

    Disable `wrap` to improve Markview table rendering

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -17,6 +17,17 @@ return {
   config = function(_, opts)
     require("markview").setup(opts);
     vim.keymap.set('n', '<leader>tm', '<CMD>Markview toggle<CR>', { desc = '[T]oggle [M]arkView' })
+
+    -- See https://github.com/OXY2DEV/markview.nvim/issues/248#issuecomment-2603697869
+    vim.api.nvim_create_autocmd('FileType', {
+      desc = 'Disable `wrap` to improve Markview table rendering',
+      pattern = { "markdown", "quarto", "rmd", },
+      group = vim.api.nvim_create_augroup('Markview_wrap_disable', { clear = true }),
+      callback = function (opts)
+        vim.o.wrap = false
+      end,
+    })
+
   end
 
 }
