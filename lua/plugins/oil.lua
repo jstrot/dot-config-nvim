7cd29918542cc3d15024de4a980d71e2b603ec63
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-02-02 01:21:52 -0500

    oil: Add extra columns handling and fix keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,31 +1,58 @@
 -- https://github.com/stevearc/oil.nvim
+local file_info_columns_displayed = false
+local min_columns = {
+  "icon",
+}
+local all_columns = {
+  "icon",
+  "permissions", -- not default
+  "size",        -- not default
+  "mtime",       -- not default
+}
+local function toggle_file_info_columns()
+  file_info_columns_displayed = not file_info_columns_displayed
+  require("oil").set_columns(file_info_columns_displayed and all_columns or min_columns)
+end
 return {
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     -- event = 'VeryLazy',  lazy-loading would break `nvim .`
     dependencies = {
       -- 'echasnovski/mini.icons',
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
     opts = {
+      columns = file_info_columns_displayed and all_columns or min_columns,
+      buf_options = {
+        buflisted = true,        -- default: false
+        bufhidden = "",          -- default: "hide"
+      },
+      watch_for_changes = false, -- default: true
+      view_options = {
+        show_hidden = true,      -- default: false
+        -- default: natural_order = "fast",
+        -- default: case_insensitive = false,
+      },
+      -- Align keymaps with Telescope
+      -- use_default_keymaps = true,  -- Don't disable unless you list all keymaps
       keymaps = {
-        -- Align keymaps with Telescope
-        ["<C-?>"] = "actions.show_help", -- default: "g?"
-        ["<CR>"] = "actions.select",
-        ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" }, -- default: "<C-s>"
-        ["<C-x>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" }, -- default: "<C-h>"
-        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
-        ["gs"] = "actions.change_sort",
-        ["gx"] = "actions.open_external",
-        ["g."] = "actions.toggle_hidden",
-        ["g\\"] = "actions.toggle_trash",
+        ["<leader>?"] = { -- default: "g?",
+          "actions.show_help",
+        },
+        ["<C-v>"] = { -- default: "<C-s>"
+          "actions.select",
+          opts = { vertical = true },
+          desc = "Open the entry under the cursor { vertical = true }",
+        },
+        ["<C-x>"] = { -- default: "<C-h>"
+          "actions.select",
+          opts = { horizontal = true },
+          desc = "Open the entry under the cursor { horizontal = true }",
+        },
+        ["<leader>tI"] = {
+          toggle_file_info_columns,
+          desc = "[T]oggle file [I]nformation columns",
+        },
       },
     },
   },
