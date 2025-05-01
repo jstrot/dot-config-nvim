2f104517ed00e70789266e741ca26021013dfa06
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-04-30 20:36:59 -0400

    Copilot: Fix lazy-loading, expose some options and default `node` path

--- lua/plugins/Copilot.lua
+++ lua/plugins/Copilot.lua
@@ -2,10 +2,35 @@
 return {
   {
     "github/copilot.vim",
-    event = 'InsertEnter',
-    cmd = {
-      'Copilot',
-    },
+
+    -- FIXME: Lazy-loading doesn't seem to work for Copilot because it always
+    -- requires the file to be re-loaded (`:e`) before getting attached.
+    -- event = 'InsertEnter',
+    -- cmd = {
+    --   'Copilot',
+    -- },
+
+    init = function()
+
+      --[[ Change the default key mapping (default is <Tab>) ]]
+      -- vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      --   expr = true,
+      --   replace_keycodes = false
+      -- })
+      -- vim.g.copilot_no_tab_map = true
+
+      vim.g.copilot_node_command = vim.g.node_host_prog or 'node'
+
+      -- vim.g.copilot_filetypes = {
+      --   c = true,
+      --   markdown = true,
+      --   python = true,
+      -- }
+      -- vim.g.copilot_filetypes['*'] = false
+
+      -- vim.g.copilot_workspace_folders = {"~/Projects/myproject"}
+
+    end,
   },
 }
 
