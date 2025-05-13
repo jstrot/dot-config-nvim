2c28de07759296d892112d588e785b8e5979cf42
Jean-Sebastien Trottier <strottie@cisco.com>
2025-05-13 16:01:59 -0400

    CopilotChat: Update model config comment

--- lua/plugins/CopilotChat.lua
+++ lua/plugins/CopilotChat.lua
@@ -19,7 +19,7 @@ return {
       -- allow_insecure = false, -- Allow insecure server connections
 
       -- system_prompt = prompts.COPILOT_INSTRUCTIONS, -- System prompt to use
-      -- model = 'gpt-4', -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
+      -- model = 'gpt-4o', -- See `:CopilotChatModels` to pick or list of models
       -- temperature = 0.1, -- GPT temperature
 
       -- question_header = '## User ', -- Header to use for user questions
