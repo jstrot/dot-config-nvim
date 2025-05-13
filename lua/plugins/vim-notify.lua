d47529390347b94f0743ae0c256364ed7bcd381d
Jean-Sebastien Trottier <strottie@cisco.com>
2025-05-13 16:10:18 -0400

    vim-notify: Fix issues with background color for transparency

--- lua/plugins/vim-notify.lua
+++ lua/plugins/vim-notify.lua
@@ -2,8 +2,16 @@
 return {
   {
     "rcarriga/nvim-notify",
+    opts = {
+      -- Highlight group 'NotifyBackground' has no background highlight
+      -- Please provide an RGB hex value or highlight group with a background value for 'background_colour' option.
+      -- This is the colour that will be used for 100% transparency.
+      background_colour = "#000000",
+    },
     config = function(_, opts)
-      vim.notify = require("notify")
+      local notify = require("notify")
+      notify.setup(opts)
+      vim.notify = notify
     end,
   },
 }
