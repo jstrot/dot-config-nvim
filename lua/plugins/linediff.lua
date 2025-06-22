7b57670bb047b4dec0212df61558aa963f8e3b47
Jean-Sebastien Trottier <jst@qualipsof.com>
2025-06-22 15:12:29 -0400

    Add Linediff

---
+++ lua/plugins/linediff.lua
@@ -0,0 +1,17 @@
+-- https://github.com/AndrewRadev/linediff.vim
+return {
+  {
+    "AndrewRadev/linediff.vim",
+    cmd = {
+      "Linediff",
+      "LinediffAdd",
+      "LinediffLast",
+      "LinediffMerge",
+      "LinediffPick",
+      "LinediffReset",
+      "LinediffShow",
+    },
+  }
+}
+
+-- vim: sw=2 et
