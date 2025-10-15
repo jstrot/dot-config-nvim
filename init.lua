b7c2311ff20667cc15ee1fec7a67bf3d748081b7
Jean-Sebastien Trottier <strottie@cisco.com>
2025-10-15 13:00:57 -0400

    <Esc>: Also temporarily clear suggestions

--- init.lua
+++ init.lua
@@ -86,7 +86,12 @@ vim.o.confirm = true
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', function()
+  vim.cmd([[nohlsearch]])
+  if package.loaded['config-lsp.nes'] then
+    require('copilot-lsp.nes').clear()
+  end
+end, { desc = "Clear highglights and suggestions" })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
