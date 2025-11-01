8b0392c428f66d30dd81eb97e30ca8f40e9d7eb2
Jean-Sebastien Trottier <strottie@cisco.com>
2025-10-31 22:29:31 -0400

    vim-figutive: Fix :Gllogfunc and update lazy loading args

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -30,24 +30,27 @@ return {
       'Gvsplit',
       'Gwq',
       'Gwrite',
+      --[[ Custom commands ]]
+      'Gclogfunc',
+      'Gllogfunc',
     },
-    config = function()
-      -- require('fugitive')  -- Auto-loading
+    keys = {
+      { '<leader>gd', '<cmd>Gvdiffsplit<CR>',        desc = '[G]it vim[D]iff file against the index' },
+      { '<leader>gD', '<cmd>Gvdiffsplit HEAD~1<CR>', desc = '[G]it vim[D]iff file against the last commit' },
+      { '<leader>ga', '<cmd>Git add -p<CR>',         desc = '[G]it [A]dd (patch mode)' },
+      { '<leader>gc', '<cmd>Git commit<CR>',         desc = '[G]it [C]commit' },
+      { '<leader>gS', '<cmd>Git stage %<CR>',        desc = '[G]it [S]tage buffer' },
+      { '<leader>gR', '<cmd>Git reset %<CR>',        desc = '[G]it [R]eset buffer' },
+    },
+    init = function()
 
       -- https://github.com/tpope/vim-fugitive/issues/1663
       vim.api.nvim_create_user_command('Gclogfunc', function()
         vim.cmd [[ execute '.Gclog -L :' . expand('<cword>') . ':%' ]]
       end, { bang = true, bar = true, })
       vim.api.nvim_create_user_command('Gllogfunc', function()
-        vim.cmd [[ execute '.Gclog -L :' . expand('<cword>') . ':%' ]]
+        vim.cmd [[ execute '.Gllog -L :' . expand('<cword>') . ':%' ]]
       end, { bang = true, bar = true, })
-
-      vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiffsplit<CR>', { desc = '[G]it vim[D]iff file against the index' })
-      vim.keymap.set('n', '<leader>gD', '<cmd>Gvdiffsplit HEAD~1<CR>', { desc = '[G]it vim[D]iff file against the last commit' })
-
-      vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>', { desc = '[G]it [C]commit' })
-      vim.keymap.set('n', '<leader>gS', '<cmd>Git stage %<CR>', { desc = '[G]it [S]tage buffer' })
-      vim.keymap.set('n', '<leader>gR', '<cmd>Git reset %<CR>', { desc = '[G]it [R]eset buffer' })
 
     end
   }
