6a6d268582de4cef2a588955293ae1fc89b81c66
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-11-10 10:26:57 -0500

    Rename Copilot -> Copilot-vim

--- lua/plugins/Copilot-vim.lua
+++ lua/plugins/Copilot-vim.lua
@@ -9,7 +9,7 @@ end
 return {
   {
     "github/copilot.vim",
-    enabled = vim.g.github_copilot_enabled,
+    enabled = vim.g.github_copilot_enabled and vim.g.auto_suggest_completion_plugin ~= 'copilot-lua', -- Avoid plugin name conflict
     cond = vim.g.auto_suggest_completion_plugin == 'copilot',
     priority = 45, -- default is 50, 45 is the preferred auto-suggest completion plugin, others are 40
 
