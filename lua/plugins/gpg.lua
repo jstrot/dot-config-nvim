bf5e941895b8953ff509ab00de8d2e583b34c3aa
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-11-16 00:29:58 -0500

    gpg: Fix macOS detection

--- lua/plugins/gpg.lua
+++ lua/plugins/gpg.lua
@@ -5,7 +5,7 @@ return {
     -- This only works if you have a working graphical pinentry, not a text pinentry
     cond = (
       vim.tbl_contains({ 'x11', 'wayland', }, vim.env.XDG_SESSION_TYPE) -- Linux
-      or vim.fn.executable('launchctl') == 1 and vim.tbl_contains({ 'Aqua', }, vim.system({'launchctl', 'managername'}):wait().stdout) -- macOS
+      or vim.fn.executable('launchctl') == 1 and vim.tbl_contains({ 'Aqua', }, vim.system({'launchctl', 'managername'}):wait().stdout ~= nil) -- macOS
     ),
   },
 }
