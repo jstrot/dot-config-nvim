f8197bf978c0ef6d52b0173fbafc8a9ae2bfb95a
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-11-16 22:44:47 -0500

    Obsidian: cleanup

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -38,38 +38,38 @@ return {
       -- 'epwalsh/pomo.nvim',  -- TODO:
     },
 
-		daily_notes = {
-			-- Optional, if you keep daily notes in a separate directory.
-			folder = "notes/dailies",
-			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%Y-%m-%d",
-			-- Optional, if you want to change the date format of the default alias of daily notes.
-			alias_format = "%B %-d, %Y",
-			-- Optional, default tags to add to each new daily note created.
-			default_tags = { "daily-notes" },
-			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-			template = 'daily.md',
-		},
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = 'daily.md',
+    },
 
-		-- -- Optional, customize how note IDs are generated given an optional title.
-		-- ---@param title string|?
-		-- ---@return string
-		-- note_id_func = function(title)
-		-- 	-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-		-- 	-- In this case a note with the title 'My new note' will be given an ID that looks
-		-- 	-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-		-- 	local suffix = ""
-		-- 	if title ~= nil then
-		-- 		-- If title is given, transform it into valid file name.
-		-- 		suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-		-- 	else
-		-- 		-- If title is nil, just add 4 random uppercase letters to the suffix.
-		-- 		for _ = 1, 4 do
-		-- 			suffix = suffix .. string.char(math.random(65, 90))
-		-- 		end
-		-- 	end
-		-- 	return tostring(os.time()) .. "-" .. suffix
-		-- end,
+    -- -- Optional, customize how note IDs are generated given an optional title.
+    -- ---@param title string|?
+    -- ---@return string
+    -- note_id_func = function(title)
+    --  -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    --  -- In this case a note with the title 'My new note' will be given an ID that looks
+    --  -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    --  local suffix = ""
+    --  if title ~= nil then
+    --    -- If title is given, transform it into valid file name.
+    --    suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    --  else
+    --    -- If title is nil, just add 4 random uppercase letters to the suffix.
+    --    for _ = 1, 4 do
+    --      suffix = suffix .. string.char(math.random(65, 90))
+    --    end
+    --  end
+    --  return tostring(os.time()) .. "-" .. suffix
+    -- end,
 
     opts = {
       workspaces = workspaces,
@@ -113,7 +113,7 @@ return {
           insert_tag = "<C-l>",
         },
       },
-			legacy_commands = false, -- https://github.com/obsidian-nvim/obsidian.nvim/wiki/Commands
+      legacy_commands = false, -- https://github.com/obsidian-nvim/obsidian.nvim/wiki/Commands
     },
     cmd = {
       'Obsidian',
