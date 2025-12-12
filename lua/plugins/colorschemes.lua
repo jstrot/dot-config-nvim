92be1bdafe7ecf9f72dbc177bd7a2e92bd87f785
Jean-Sebastien Trottier <strottie@cisco.com>
2025-12-12 17:53:04 -0500

    feat(catppuccin): Enable integrations

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -11,6 +11,31 @@ return {
       dim_inactive = {
         -- enabled = true,
       },
+      integrations = {
+        blink_pairs = true,
+        diffview = true,
+        fzf = vim.g.picker_plugin == 'fzf-lua',
+        -- gitsigns = true,
+        gitsigns = {
+          enabled = true,
+          -- align with the transparent_background option by default
+          transparent = false,
+        },
+        copilot_vim = vim.g.github_copilot_enabled and vim.g.auto_suggest_completion_plugin ~= 'copilot-lua',
+        -- TODO: blink_indent = true, 'saghen/blink.indent'
+        render_markdown = true,
+        snacks = {
+          enabled = vim.g.picker_plugin == 'snacks',
+          indent_scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+        },
+        telescope = {
+          enabled = vim.g.picker_plugin == 'telescope',
+        },
+        which_key = true,
+      },
+    },
+    dependencies = {
+      -- 'Saghen/blink.pairs', -- `blink_pairs` integration
     },
   },
   {
