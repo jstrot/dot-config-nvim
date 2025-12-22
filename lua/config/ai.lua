55642be1e4e55c43f818baa516d99300a88dd247
Jean-Sebastien Trottier <jst@qualipsoft.com>
2025-12-22 15:17:54 -0500

    fix(ai): Revert inadvertent disabling of Copilot

--- lua/config/ai.lua
+++ lua/config/ai.lua
@@ -22,7 +22,7 @@
 -- - lua/plugins/opencode-native.lua
 
 vim.g.github_copilot_enabled = true -- Main switch
-vim.g.github_copilot_active = false -- Turn off temporarily if your free plan runs out
+vim.g.github_copilot_active = true -- Turn off temporarily if your free plan runs out
 vim.g.github_copilot_chat_model = nil -- nil for default, `:CopilotChatModels` to pick or list of models
 vim.g.github_copilot_code_model = nil -- nil for default
 vim.g.github_copilot_agent_model = nil -- nil for default
