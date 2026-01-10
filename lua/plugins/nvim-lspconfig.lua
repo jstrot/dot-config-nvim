7cd9e7853b1db7259ae1985690db6745b72e6d5b
Jean-Sebastien Trottier <jst@qualipsoft.com>
2026-01-10 12:05:59 -0500

    fix(nvim-lspconfig): Fix LSP configs not being applied (again!)

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -570,7 +570,7 @@ return {
       }
 
       for server, server_opts in pairs(servers) do
-        vim.lsp.config(server, server_opts)
+        vim.lsp.enable(server, server_opts)
       end
 
       -- You can add other tools here that you want Mason to install
